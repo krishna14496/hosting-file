48a72707a42326b51a2e04bbfe75aa8902b46750
Pandey <ashish.pandey@capgemini.com>
2020-04-26 05:46:46 +0530

    Updates

---
+++ precache-manifest.f404c62fd7d3ac0076a2e671851c0fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2feebb89901986e3e8dd7991d589ea8",
+    "url": "/hosting-file/index.html"
+  },
+  {
+    "revision": "6637509fe7d1d075f321",
+    "url": "/hosting-file/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9ac56a02b879d2ac465",
+    "url": "/hosting-file/static/js/2.58d9c653.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hosting-file/static/js/2.58d9c653.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6637509fe7d1d075f321",
+    "url": "/hosting-file/static/js/main.99e9c257.chunk.js"
+  },
+  {
+    "revision": "3e1cd96e1cfd34e2fc95",
+    "url": "/hosting-file/static/js/runtime-main.a75aefca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hosting-file/static/media/logo.5d5d9eef.svg"
+  }
+]);
